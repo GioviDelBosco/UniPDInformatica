53bb5d95898d4b08f4a456c64a48042c92f9d102
GioviDelBosco <giovisgara@gmail.com>
2021-11-18 18:33:19 +0100

    Completato l'esercizio 1 con tutti e 3 i punti, esercizio base per Array

--- Array/EsercizioLab5.1/MyFirstArrayTest.java
+++ Array/EsercizioLab5.1/MyFirstArrayTest.java
@@ -25,5 +25,25 @@ public class MyFirstArrayTest {
         for (int i = primoArray.length-1; i >= 0; i--) {
             System.out.print(primoArray[i]+" | ");
         }
+
+        System.out.println("");
+        System.out.println("Array modificato dopo il metodo statico \"incrementAll()\"");
+        //passo il mio array e lo vado ad utilizzare nel metodo statico
+        incrementAll(primoArray);
+
+        for (int i = 0; i < primoArray.length; i++) {
+            System.out.print(primoArray[i]+" | ");
+            
+        }
     }
+    //il mio array verra cambiato in a[] 
+    public static void incrementAll(int[] a) {
+        
+        for (int i = 0; i < a.length; i++) {
+            a[i]=a[i]+1;
+        }
+
+    }
+
+    //la parte c, nel main non verrà cambiato perché gli int sono tipi primitivi e quindi vengono cambiati solo nel metodo statico
 }
